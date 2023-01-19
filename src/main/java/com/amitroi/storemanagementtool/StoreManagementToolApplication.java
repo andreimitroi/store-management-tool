487ede5adbf6a4b40e98584e6707768c0b8eaba2
Andrei Mitroi <andreimitroi19@gmail.com>
2023-01-19 09:49:55 +0200

    Initial commit - empty project

---
+++ src/main/java/com/amitroi/storemanagementtool/StoreManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.amitroi.storemanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+	}
+
+}
